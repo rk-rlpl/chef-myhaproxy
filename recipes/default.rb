5e64ebd6a29714f959bcf1861ee70132a522f0d1
Raj Kiran Gade <rajkiran.gade@gmail.com>
2017-02-10 10:05:05 +0530

    Add two services

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,17 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+node.default['haproxy']['members']= [
+{
+ "hostname" => "default-web-server1",
+ "ipaddress" =>  "192.168.33.53",
+ "port" => 80,
+ "ssl_port" => 80 
+}, {
+ "hostname" => "default-web-server2",
+ "ipaddress" =>  "192.168.33.63",
+ "port" => 80,
+ "ssl_port" => 80 
+}]
+
+include_recipe "haproxy::default"
